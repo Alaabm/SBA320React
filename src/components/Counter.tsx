cbb846afeafcd8455ae6e00ddd1897f8853495e4
Alaabm <alaabasim@gmail.com>
2024-02-13 22:37:15 -0500

    counter code update

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,34 +2,91 @@ import{useState} from 'react'
 import "../styles/Dhikr.css";
 
 
-    
-const Counter = () => {
-    const [count, setCount] = useState(0)
-    
+const Counter = ({index}) => {
+    const [count ,setCount = useState([0, 0, 0 ])]
 
-    let incrementCount = () => {
-        setCount(count +1);
+    let incrementCount = (position) => {
+        setCount(prevCount => {
+            const newCount = [...prevCount];
+            newCount[position]+++;
+            return newCount;
+        });
     };
 
-    let decrementCount = () => {
-        setCount(count -1);
+    let decrementCount = (position) => {
+        setCount(prevCount => {
+            const newCount = [...prevCount];
+            newCount[position] --;
+            return newCount;
+        });
     };
-
 
   return (
     <>
-        <div className= "counter">
+        <div className = "counter">
             <div>
-                <h1>{count}</h1>
+                <h1>{count[index]}</h1>
             </div>
-            <div className = "buttons">
-                <button onClick={decrementCount}> - </button>
-                <button onClick={incrementCount}> + </button>
+            <div className="buttons">
+                <div>
+                    <button 
+                    onClick={() => {
+                        decrementCount(index);
+                    }}
+                    >
+                        {" "}
+                        -{" "}
+                    </button>
+                    <button
+                    onClick={() => {
+                        incrementCount(index);
+                    }}
+                    >
+                        {" "}
+                        +{" "}
+                    </button>
+                </div>
             </div>
         </div>
-         
     </>
   )
 }
 
 export default Counter
+    
+
+
+
+
+
+//my code
+// const Counter = () => {
+//     const [count, setCount] = useState(0)
+    
+
+//     let incrementCount = () => {
+//         setCount(count +1);
+//     };
+
+//     let decrementCount = () => {
+//         setCount(count -1);
+//     };
+
+
+//   return (
+//     <>
+//         <div className= "counter">
+//             <div>
+//                 <h1>{count}</h1>
+//             </div>
+//             <div className = "buttons">
+//                 <button onClick={decrementCount}> - </button>
+//                 <button onClick={incrementCount}> + </button>
+//             </div>
+//         </div>
+         
+//     </>
+//   )
+// }
+
+// export default Counter
--- src/pages/Dhikr.tsx
+++ src/pages/Dhikr.tsx
@@ -3,30 +3,60 @@ import Counter from "../components/Counter";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
+
 const Dhikr = () => {
   return (
     <>
       <div>
-        <NavBar/>
-      <div>
-        <h1 >أستغفر الله</h1>
-        <Counter/>
-      </div>
-      <div>
-        <h1>الحمدلله</h1>
-        <Counter/>
-      </div>
-      <div>
-      <h1>الله أكبر</h1>
+        <NavBar />
+        <div>
+          <h1>أستغفر الله</h1>
+          <Counter index={0} />
+        </div>
+        <div>
+          <h1>الحمدلله</h1>
+          <Counter index={1} />
+        </div>
+        <div>
+          <h1>الله أكبر</h1>
+          <Counter index={2} />
+        </div>
 
-        <Counter/>
+        <Footer />
       </div>
-      
-      <Footer/>
-      </div>
-      
     </>
-  )
-}
+  );
+};
+
 
 export default Dhikr
+
+//My code
+
+// const Dhikr = () => {
+//   return (
+//     <>
+//       <div>
+//         <NavBar/>
+//       <div>
+//         <h1 >أستغفر الله</h1>
+//         <Counter/>
+//       </div>
+//       <div>
+//         <h1>الحمدلله</h1>
+//         <Counter/>
+//       </div>
+//       <div>
+//       <h1>الله أكبر</h1>
+
+//         <Counter/>
+//       </div>
+      
+//       <Footer/>
+//       </div>
+      
+//     </>
+//   )
+// }
+
+// export default Dhikr
